1e4cbf213e615f120cf48ca87f3de326f624e30c
Chih-hao Chen <dchen@wisdome.ai>
2023-09-26 22:12:05 +0800

    (feat) add google oauth sign up function
    [how]
     - create login.html
     - use passport and passport-google-oauth20 for auth google API

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
-import express from 'express';
+import path from 'path';
+
+import express, {Request, Response} from 'express';
+import passport from 'passport';
 import 'dotenv/config';
+
+import authRouter from './routes/auth';
 
 const app = express();
 const port = process.env.PORT || 3000;
+const viewsDir = path.join(__dirname, '../views');
+
+app.use(passport.initialize());
+app.use('/auth', authRouter);
+
+app.use('/', (req: Request, res: Response) => {
+  console.log('root');
+  return res.sendFile('login.html', {root: viewsDir});
+});
 
 app.listen(port, () => {
   console.log(`Server is listening at prot:${port}`);
