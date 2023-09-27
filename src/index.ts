3917a82c56d0b88be320d473892032c931b175cc
Chih-hao Chen <dchen@wisdome.ai>
2023-09-27 10:17:46 +0800

    (feat) add session and store cookies for google oauth user
    [how] use express-session and store user email

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import path from 'path';
 
-import express, {Request, Response} from 'express';
+import express, {Request, Response, NextFunction} from 'express';
 import passport from 'passport';
+import GoogleStrategy from 'passport-google-oauth20';
 import 'dotenv/config';
+import session from 'express-session';
 
 import authRouter from './routes/auth';
 
@@ -10,12 +12,50 @@ const app = express();
 const port = process.env.PORT || 3000;
 const viewsDir = path.join(__dirname, '../views');
 
+// session
+declare module 'express-session' {
+  interface SessionData {
+    email: string;
+  }
+}
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET as string,
+    saveUninitialized: false,
+    resave: true,
+  })
+);
+
+// passport
+passport.use(
+  new GoogleStrategy.Strategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      callbackURL: `http://${process.env.DOMAIN}:${process.env.PORT}/auth/google/callback`,
+    },
+    (accessToken, refreshToken, profile, cb) => {
+      // TODO: store user data to database
+      return cb(null, profile);
+    }
+  )
+);
 app.use(passport.initialize());
+
 app.use('/auth', authRouter);
 
-app.use('/', (req: Request, res: Response) => {
-  console.log('root');
-  return res.sendFile('login.html', {root: viewsDir});
+function auth(req: Request, res: Response, next: NextFunction) {
+  if (req.session.email) {
+    // TODO: check email in database
+    next();
+  } else {
+    return res.sendFile('login.html', {root: viewsDir});
+  }
+}
+
+app.use('/', auth, (req: Request, res: Response) => {
+  return res.sendFile('dashboard.html', {root: viewsDir});
 });
 
 app.listen(port, () => {
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,34 +1,24 @@
 import express from 'express';
 import passport from 'passport';
-import passportGoogleOAuth2 from 'passport-google-oauth20';
+import GoogleStrategy from 'passport-google-oauth20';
 import 'dotenv/config';
 
 const router = express.Router();
-
-passport.use(
-  new passportGoogleOAuth2.Strategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      callbackURL: `http://${process.env.DOMAIN}:${process.env.PORT}/auth/google/callback`,
-    },
-    (accessToken, refreshToken, profile, cb) => {
-      return cb(null, profile);
-    }
-  )
-);
 
 router.get(
   '/google',
   passport.authenticate('google', {scope: ['profile', 'email']})
 );
 
-// 處理 Google OAuth 2.0 回調
 router.get(
   '/google/callback',
   passport.authenticate('google', {failureRedirect: '/', session: false}),
   (req, res) => {
-    res.send('Login success');
+    const user = req.user as GoogleStrategy.Profile;
+    if (user.emails) {
+      req.session.email = user.emails[0].value;
+    }
+    res.redirect('/');
   }
 );
 
