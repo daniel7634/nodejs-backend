7943da2bd0e2e7aaf6d82f631e8d4f9089e36b33
Chih-hao Chen <dchen@wisdome.ai>
2023-10-03 09:54:26 +0800

    (session): use MySQL for session
    - setup session store
    - modify the ORM model

--- src/validators/userValidator.ts
+++ src/validators/userValidator.ts
@@ -1,7 +1,12 @@
 import {Meta, body} from 'express-validator';
 
 export const profileNameValidator = () =>
-  body('name').trim().notEmpty().withMessage('The name can be empty');
+  body('name')
+    .trim()
+    .notEmpty()
+    .withMessage('The name can be empty')
+    .isLength({max: 30})
+    .withMessage('The name must not exceed 30 characters');
 
 const checkPasswordChange = (newPassword: string, meta: Meta): boolean => {
   const {oldPassword} = meta.req.body;
@@ -15,7 +20,10 @@ export const resetPasswordValidators = [
   body('oldPassword')
     .trim()
     .notEmpty()
-    .withMessage('The old password is required')
+    .withMessage('Password is required')
+    .bail()
+    .isLength({max: 30})
+    .withMessage('Password must not exceed 30 characters')
     .bail()
     .isStrongPassword({
       minLength: 8,
@@ -24,11 +32,14 @@ export const resetPasswordValidators = [
       minNumbers: 1,
       minSymbols: 1,
     })
-    .withMessage('The old password does not meet the requirements'),
+    .withMessage('Password does not meet the requirements'),
   body('newPassword')
     .trim()
     .notEmpty()
-    .withMessage('The new password is required')
+    .withMessage('Password is required')
+    .bail()
+    .isLength({max: 30})
+    .withMessage('Password must not exceed 30 characters')
     .bail()
     .isStrongPassword({
       minLength: 8,
@@ -37,7 +48,7 @@ export const resetPasswordValidators = [
       minNumbers: 1,
       minSymbols: 1,
     })
-    .withMessage('The new password does not meet the requirements')
+    .withMessage('Password does not meet the requirements')
     .custom(checkPasswordChange)
     .withMessage('The new password must be different'),
 ];
