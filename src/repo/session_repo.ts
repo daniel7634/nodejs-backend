e643d4e2225f22ae31cc02a931c4770e0bf95f68
Chih-hao Chen <dchen@wisdome.ai>
2023-10-09 08:44:05 +0800

    (refactor): rename function and remove no used function

--- src/repo/session_repo.ts
+++ src/repo/session_repo.ts
@@ -2,42 +2,31 @@ import {PrismaClient} from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export async function getTodaySessionCount(): Promise<number> {
-  const today = new Date();
-  today.setHours(0, 0, 0, 0);
-
-  const tomorrow = new Date(today);
-  tomorrow.setDate(tomorrow.getDate() + 1);
-
-  const count = await prisma.session.count({
-    where: {
-      updatedAt: {
-        gte: today,
-        lt: tomorrow,
-      },
-    },
-  });
-  return count;
-}
-
-export async function getSessionCountByDay(day: number): Promise<number> {
-  if (day < 1) {
-    return 0;
-  }
-
+function getStartAndEndDates(daysAgo = 1): [Date, Date] {
   const today = new Date();
   today.setUTCHours(0, 0, 0, 0);
 
-  const startDate = new Date(today);
-  startDate.setDate(startDate.getDate() - (day - 1));
-  const tomorrow = new Date(today);
-  tomorrow.setDate(tomorrow.getDate() + 1);
+  const start = new Date(today);
+  start.setDate(start.getDate() - (daysAgo - 1));
+
+  const end = new Date(today);
+  end.setDate(end.getDate() + 1);
+
+  return [start, end];
+}
+
+export async function getSessionCountDaysAgo(daysAgo: number): Promise<number> {
+  if (daysAgo < 1) {
+    return 0;
+  }
+
+  const [startDate, endDate] = getStartAndEndDates(daysAgo);
 
   const count = await prisma.session.count({
     where: {
       updatedAt: {
         gte: startDate,
-        lt: tomorrow,
+        lt: endDate,
       },
     },
   });
--- src/services/dashboard_service.ts
+++ src/services/dashboard_service.ts
@@ -1,4 +1,4 @@
-import {getSessionCountByDay} from '../repo/session_repo';
+import {getSessionCountDaysAgo} from '../repo/session_repo';
 import {getUserCount, getUsers} from '../repo/user_repo';
 
 export async function getTotalSignedUp(): Promise<number> {
@@ -6,14 +6,14 @@ export async function getTotalSignedUp(): Promise<number> {
 }
 
 export async function getTotalSessionToday(): Promise<number> {
-  return await getSessionCountByDay(1);
+  return await getSessionCountDaysAgo(1);
 }
 
 export async function getAverageSession(day: number): Promise<number> {
   if (day < 1) {
     return 0;
   }
-  return parseFloat(((await getSessionCountByDay(day)) / day).toFixed(2));
+  return parseFloat(((await getSessionCountDaysAgo(day)) / day).toFixed(2));
 }
 
 export async function getUsersPagination(page: number) {
